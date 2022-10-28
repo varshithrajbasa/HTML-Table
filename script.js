dada608fe2f629087854dc2e6f199b413d6ebead
krishna120801 <57492276+krishna120801@users.noreply.github.com>
2022-10-28 23:28:05 +0530

    Sort button added below table

--- script.js
+++ script.js
@@ -43,5 +43,27 @@ function filter(el){
 
 }
 
-function sort(){}
+function sort(){
+	 var filterTable, rows, sorted, i, x, y, sortFlag;
+      filterTable = document.querySelector(".table");
+      sorted = true;
+      while (sorted) {
+         sorted = false;
+         rows = filterTable.rows;
+         for (i = 1; i < rows.length - 1; i++) {
+            sortFlag = false;
+            x = rows[i].getElementsByTagName("td")[0];
+            y = rows[i + 1].getElementsByTagName("td")[0];
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+               sortFlag = true;
+               break;
+            }
+         }
+         if (sortFlag) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            sorted = true;
+         }
+      }
+	
+}
 function paginate(){}
